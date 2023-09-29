11aaa3a3aff712c765fee0ca5d5fa41ae4e04569
ruben-hutter <hutter.ruben@gmail.com>
2023-09-29 19:55:39 +0200

    Add exercise 1 up to task 3

---
+++ exercise_1/ex-1.md
@@ -0,0 +1,9 @@
+# Exercise sheet 1
+
+## Task (Practical) 2: Interaction
+
+There are 50,772,456 games in the database.
+
+## Task (Theory) 3: Questions
+
+
---
+++ exercise_1/plotter.py
@@ -0,0 +1,57 @@
+import numpy as np
+import psycopg2
+import matplotlib.pyplot as plt
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    dbname="demo",
+    user="demo",
+    password="demo",
+    host="localhost",
+    port="54321"
+)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# Execute SQL query
+cur.execute("""
+    SELECT game_month,
+           SUM(game_white_wins::integer) AS white_wins,
+           SUM((NOT game_white_wins)::integer) AS black_wins
+    FROM games_simple
+    GROUP BY game_month
+""")
+
+# Fetch all the rows
+rows = cur.fetchall()
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+
+# Unpack the result into separate lists
+months, white_wins, black_wins = zip(*rows)
+
+# Define the width of each bar
+bar_width = 0.4
+
+# Generate an array of indices for each month
+indices = np.arange(len(months))
+
+# Create a grouped bar chart
+plt.figure(figsize=(10, 5))
+plt.bar(indices, white_wins, color='b', width=bar_width, label='White Wins')
+plt.bar(indices + bar_width, black_wins, color='r', width=bar_width, label='Black Wins')
+
+# Set the positions of the x-axis ticks and labels
+plt.xticks(indices + bar_width/2, months)
+
+# Add labels and title
+plt.xlabel('Month')
+plt.ylabel('Number of Wins')
+plt.title('White vs. Black Wins per Month')
+plt.legend()
+
+# Show the plot
+plt.show()
