ad4267a198b4164faac24a6d0cf8f4e11f33b4c4
ruben-hutter <hutter.ruben@gmail.com>
2023-12-22 10:37:46 +0100

    Update size comparison script

--- exercise_5/size_comparison.sh
+++ exercise_5/size_comparison.sh
@@ -5,10 +5,3 @@ csv_size=$(du -h 100127.csv | cut -f1)
 echo "SQLite DB: $db_size"
 echo "CSV file: $csv_size"
 
-if [ "$db_size" -eq "$csv_size" ]; then
-    echo "Same size"
-elif [ "$db_size" -lt "$csv_size" ]; then
-    echo "DB < CSV"
-else
-    echo "CSV < DB"
-fi
