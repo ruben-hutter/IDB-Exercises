520d1608ea57f270d237e3f0b433c6907fa2f388
ruben-hutter <hutter.ruben@gmail.com>
2024-10-16 11:16:37 +0200

    Fix script for task 1

--- HS2024/ex2/task1.py
+++ HS2024/ex2/task1.py
@@ -4,6 +4,9 @@ import psycopg2
 # Get password from environment variable
 password = os.environ.get("DB_PASSWORD")
 
+# Separator
+separator = "-" * 30
+
 # Connect to PostgreSQL
 con = psycopg2.connect(
     dbname="introdb",
@@ -17,26 +20,28 @@ with con.cursor() as cur:
     # Print how many games were won by white
     cur.execute("""\
         SELECT COUNT(*)
-        FROM games_simple
-        WHERE winner = 'white'
+        FROM games
+        WHERE result = '1-0'
     """)
     white_wins = cur.fetchone()
     if white_wins:
         print("White wins:", white_wins[0])
 
+    print(separator)
     # Print the number of games per month. Print the name of the month, not the number
     cur.execute("""\
-        SELECT EXTRACT(MONTH FROM date) AS month, COUNT(*)
-        FROM games_simple
+        SELECT EXTRACT(MONTH FROM game_start) AS month, COUNT(*)
+        FROM games
         GROUP BY month
     """)
     for month, count in cur.fetchall():
         print("Month:", month, "Count:", count)
 
+    print(separator)
     # Print the total number of games
     cur.execute("""\
         SELECT COUNT(*)
-        FROM games_simple
+        FROM games
     """)
     total_games = cur.fetchone()
     if total_games:
